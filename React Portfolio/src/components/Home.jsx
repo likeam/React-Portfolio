8d0557d6e1e3c077dceeca5b9ba38c85b4e8eb3b
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-06 18:35:54 +0500

    Building the Home Page

--- React Portfolio/src/components/Home.jsx
+++ React Portfolio/src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import second from '../assets/AbdulRehman.jpg'
+import second from '../assets/AbdulRhman.png'
+
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
+
+
   return (
     <div name="home" className=' h-screen w-full bg-gradient-to-b from-black  via-black to-gray-800 '>        
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full p-4 md:flex-row '>
@@ -18,7 +21,7 @@ const Home = () => {
                 <div>
                     <button className='text-white w-fit px-6 py-3 my-2 flex items-center bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer  ' >
                         Portfolio
-                        <span className='group-hover:rotate-90 duration-300'>
+                        <span className='hover:rotate-90 duration-300'>
                             <MdOutlineKeyboardArrowRight size={25} /> 
                         </span>
                     </button>
--- React Portfolio/src/components/SocialLinks.jsx
+++ React Portfolio/src/components/SocialLinks.jsx
@@ -5,10 +5,56 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 
 
 const SocialLinks = () => {
+
+    
+    const links = [
+        {
+         id: 1,
+         child: (
+            <>
+                LinkdIn<FaLinkedin size={30} />
+            </>
+         ),
+         href: "https://linkedin.com",
+         style: "rounded-tr-md",
+        },
+        {
+            id: 2,
+            child: (
+               <>
+                   GitHub<FaGithub size={30} />
+               </>
+            ),
+            href: "https://github.com",
+           },
+           {
+            id: 3,
+            child: (
+               <>
+                   Mail<HiOutlineMail size={30} />
+               </>
+            ),
+            href: "milto:likeam99.gmail.com",
+           },
+           {
+            id: 1,
+            child: (
+               <>
+                   Resume<BsFillPersonLinesFill size={30} />
+               </>
+            ),
+            href: "/resume.pdf",
+            style: "rounded-tr-md",
+            download:true,
+           },
+ 
+     ]
+
+
   return (
     <div className='flex-col top-[35%] left-0 fixed ' >
         <ul>
-            <li className='flex justify-between items-center w-40 h-1/4 px-4  bg-gray-400 '>
+            <li className='ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 flex justify-between items-center w-40 h-14 px-4  bg-gray-400 '>
             <a href='' className='flex justify-between items-center w-full text-white' >
                 <>
                     LinkdIn<FaLinkedin size={30} />
