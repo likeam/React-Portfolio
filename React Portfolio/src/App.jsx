bac14f68acbeee72de6c8dbfed7edf520f11ec24
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-07 10:14:32 +0500

    Bulding Portfolio Section

--- React Portfolio/src/App.jsx
+++ React Portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
+import Portfolio from "./components/Portfolio";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
     <Navbar />
     <Home />
     <About />
+    <Portfolio />
     <SocialLinks />
   </div>
  
