2db2e9df2b0f3c6cad3be5819e6b71b538eed985
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-03-07 11:09:38 +0500

    Comleting Portfolio

--- React Portfolio/src/components/Portfolio.jsx
+++ React Portfolio/src/components/Portfolio.jsx
@@ -10,30 +10,66 @@ import usestate from '../assets/portfolio/usestate.jpg';
 
 
 const Portfolio = () => {
+
+    const pportfolios =[
+        {
+            id: 1,
+            src: arrayDestruct,
+        },
+        {
+            id: 2,
+            src:installNode, 
+        },
+        {
+            id: 3,
+            src: navbar,
+        },
+        {
+            id: 4,
+            src: reactParallax,
+        },
+        {
+            id: 5,
+            src: reacSmooth,
+        },
+        {
+            id: 6,
+            src: reactWeather,
+        },
+        
+    ]
   return (
-    <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full' >
+    <div name="portfolio" 
+         className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-scree"    
+        >
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full" >
             <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                <p className="text-4xl font-bold inline border-b-4 border-gray-500">
                     Portfolio
                 </p>
                 <p className='py-6'>
                     Check out some of my work right here
                 </p>
             </div>
-             <div className='grid sm:grid-cols-2 md: grid-cols-3 gap-8 px-12 sm:px-0'>
-                <div className='shadow-md shadow-gray-600 rounded-lg'  />
-                    <img   
-                        src={reactWeather} alt='weather'       
-                        className='rounded-md duration-200 hover:scale-110'                 
-                    />
-                </div>
-                <div className='flex justify-center items-center'>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110'>Demo</button>
-                    <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110'>Code</button>
-                </div>
-             </div>
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+                {
+                    pportfolios.map(({id, src}) => (
+                        <div key={id} className='shadow-md shadow-gray-600 rounded-lg'  >
+                            <img   
+                                src={src} alt='weather'       
+                                className='rounded-md duration-200 hover:scale-110'                 
+                            />                    
+                            <div className='flex justify-center items-center'>
+                                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110'>Demo</button>
+                                <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110'>Code</button>
+                            </div>
+                         </div>              
+            
+                    ))
+                }
+            </div>
         </div>
+    </div>
   )
 }
 
