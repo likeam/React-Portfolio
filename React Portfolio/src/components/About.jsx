3c26c79e2dbee38bd1ae96daaf8646d95e65b40b
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-05-27 10:49:37 +0500

    Removing Errors

--- React Portfolio/src/components/About.jsx
+++ React Portfolio/src/components/About.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white' >
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full '>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
-            </div>
-            <p className='text-xl mt-20'>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-                deserunt illum mollitia officiis qui exercitationem perferendis neque
-                quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-                iste, totam fugiat recusandae rerum laborum perferendis molestiae
-                aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-                ab mollitia voluptatibus, a nostrum eveniet laborum!
-            </p>
-            <br />
-            <p className='text-xl'>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius,
-          deserunt illum mollitia officiis qui exercitationem perferendis neque
-          quasi a recusandae necessitatibus tempora iusto! Blanditiis error
-          iste, totam fugiat recusandae rerum laborum perferendis molestiae
-          aperiam asperiores nemo. Magni dolor maxime debitis vitae, eaque hic
-          ab mollitia voluptatibus, a nostrum eveniet laborum!
-            </p>
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
+        <p className="text-xl mt-20">
+          Highly skilled and motivated MERN Full Stack Developer with 4 years of
+          experience in designing, developing, and deploying scalable web
+          applications. Proficient in building responsive front-end interfaces
+          using React.js and robust back-end systems using Node.js and
+          Express.js, with MongoDB for database management. Adept at
+          collaborating with cross-functional teams to deliver high-quality
+          software solutions. Passionate about writing clean, maintainable code
+          and staying updated with the latest web development trends.
+        </p>
+        <br />
+        <p className="text-xl">
+          Front-End Development: React.js, Next.js TypeScript, HTML5, CSS3,
+          JavaScript (ES6+), Redux, Bootstrap, Tailwind CSS, Material-UI
+          <br />
+          Back-End Development: Node.js, Express.js, RESTful APIs, GraphQL,
+          Python Database Management: MongoDB, Mongoose, Firebase, MySql
+          <br />
+          Version Control: Git, GitHub, GitLab
+          <br />
+          DevOps & Deployment: Docker, AWS, Heroku, Netlify, Vercel Tools:
+          Postman, VS Code, Webpack, Babel, NPM/Yarn
+          <br />
+          Other Skills: Agile Methodology, Scrum, Problem-Solving, Debugging
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- React Portfolio/src/components/Experience.jsx
+++ React Portfolio/src/components/Experience.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import html from "../assets/html.png";
-import css from "../assets/css.png";
+import React from "react";
+import next from "../assets/next.png";
+
 import javascript from "../assets/javascript.png";
 import reactImage from "../assets/react.png";
 import native from "../assets/native.png";
 import node from "../assets/node.png";
-import github from "../assets/github.png";
+import express from "../assets/Express.png";
 import tailwind from "../assets/tailwind.png";
 import typescript from "../assets/typescript.png";
+import mongogdb from "../assets/MongoDB.png";
+import mysql from "../assets/MySQL.png";
+import postgres from "../assets/PostgresSQL.png";
+import python from "../assets/Python.png";
 
 const Experience = () => {
-
   const techs = [
     {
       id: 1,
-      src: html,
-      title: "HTML",
-      style: "shadow-orange-500",
+      src: next,
+      title: "NEXT",
+      style: "shadow-gray-500 ",
     },
     {
       id: 2,
-      src: css,
-      title: "CSS",
-      style: "shadow-blue-500",
+      src: reactImage,
+      title: "React",
+      style: "shadow-blue-600",
     },
     {
       id: 3,
@@ -32,53 +35,74 @@ const Experience = () => {
     },
     {
       id: 4,
-      src: reactImage,
-      title: "React",
-      style: "shadow-blue-600",
+      src: typescript,
+      title: "TypeScript",
+      style: "shadow-blue-400",
     },
     {
       id: 5,
       src: tailwind,
       title: "Tailwind",
-      style: "shadow-sky-400",
+      style: "shadow-red-400",
     },
     {
       id: 6,
       src: native,
       title: "Native JS",
-      style: "shadow-white",
+      style: "shadow-purple-400",
     },
     {
       id: 7,
       src: node,
       title: "Node Js",
-      style: "shadow-pink-400",
+      style: "shadow-lime-400",
     },
     {
       id: 8,
-      src: github,
-      title: "GitHub",
-      style: "shadow-gray-400",
+      src: express,
+      title: "Express Js",
+      style: "shadow-cyan-400",
     },
     {
       id: 9,
-      src: typescript,
-      title: "TypeScript",
+      src: mongogdb,
+      title: "Mongoose",
       style: "shadow-gray-400",
+    },
+    {
+      id: 10,
+      src: mysql,
+      title: "MySQL",
+      style: "shadow-orange-400",
+    },
+    {
+      id: 11,
+      src: postgres,
+      title: "PostgresSQL",
+      style: "shadow-red-400",
+    },
+    {
+      id: 12,
+      src: python,
+      title: "Python",
+      style: "shadow-yellow-400",
     },
   ];
 
-
-
   return (
-    <div name= "experience" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black'>
-      <div className='flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto text-white'>
-        <div >
-          <p className='inline p-2 text-4xl font-bold border-b-4 border-gray-500'>Experience</p>
-          <p className='py-6'>These are the technologies I've worked with</p>
+    <div
+      name="experience"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black"
+    >
+      <div className="flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto text-white">
+        <div>
+          <p className="inline p-2 text-4xl font-bold border-b-4 border-gray-500">
+            Experience
+          </p>
+          <p className="py-6">These are the technologies I've worked with</p>
         </div>
-        <div  className="grid w-full grid-cols-2 gap-8 px-12 py-8 text-center sm:grid-cols-3 sm:px-0">
-        {techs.map(({ id, src, title, style }) => (
+        <div className="grid w-full grid-cols-2 gap-8 px-12 py-8 text-center sm:grid-cols-3 sm:px-0">
+          {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
@@ -90,7 +114,7 @@ const Experience = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
